436d48ccb4746663953090e1c928850dec6446e1
KaranChadha10 <karan.chadha@invatu.in>
2020-11-03 08:22:45 +0530

    first commit

---
+++ JwtDemo/Authentication/UserRoles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtDemo.Authentication
+{
+    public static class UserRoles
+    {
+        public const string User = "User";
+        public const string Admin = "Admin";
+
+    }
+}
